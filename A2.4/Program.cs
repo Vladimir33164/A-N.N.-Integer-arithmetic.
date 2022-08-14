33912b5820c4f0460a4e4467e2a87f3c73d189f1
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-14 19:17:54 +0300

    дано трёхзначное число. найти число полученное справа налево

---
+++ A2.4/Program.cs
@@ -0,0 +1,19 @@
+﻿//А2.4  Дано трехзначное число. Найти число, полученное при прочтении его цифр справа налево.
+
+static void Main()
+{
+    System.Console.WriteLine("Enter a three-digit number");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n > 100 && n <= 999)
+    {
+        int number = n % 100;
+        int n1 = n / 100;
+        int n2 = number / 10;
+        int n3 = number % 10;
+        System.Console.WriteLine($"{n3}{n2}{n1}");
+    }
+    else
+    {
+        System.Console.WriteLine("error.");
+    }
+}
