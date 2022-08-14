6440f95f61c7ea9c72f552e6d868ccdd4e403fab
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-14 20:16:28 +0300

    дано двухзначное число. извлечь образованное при перестановке число

---
+++ A2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿// A2.2. Дано двузначное число. Получить число, образованное при перестановке цифр заданного числа.
+
+Console.WriteLine($"Enter integer");
+      string digit = Console.ReadLine();
+      for (int i = digit.Length; i > 0; i--)
+      {
+         Console.Write(digit[i-1]);
+      }
+Console.Write("Enter a two-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n1 = number % 10;
+int n2 = number / 10;
+int su = n1 + n2;
+int pr = n1 * n2;
+int revers = n1 * 10 + n2;
+Console.WriteLine($"Dozens: {n2}, units: {n1},");
+Console.WriteLine($"sum: {su}, product: {pr},");
+Console.WriteLine($"rearrangement {revers}");
