93042cd93b6a44876f8a96da61784a5648e98e88
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-14 19:36:09 +0300

    дано трёхзначное число. получить 6 чисел с перестановкой

---
+++ A2.9/Program.cs
@@ -0,0 +1,22 @@
+﻿//A2.9. Дано трехзначное число, в котором все цифры различны.
+//Получить шесть чисел, образованных при перестановке цифр заданного числа.
+
+Console.WriteLine("Enter any integer: ");
+      string? digit = Console.ReadLine();
+      int number = int.Parse(digit);
+      int index;
+      int[] numbers = new int[digit.Length];
+
+      for (int i = 0; i < digit.Length; i++)
+      {
+        index = number % 10;
+        numbers[i] = index;
+        number /= 10;
+      }
+      int[] finalArray = new int[numbers.Length * 2];
+      Random random = new Random();
+      for (int i = 0; i < finalArray.Length; i++)
+      {
+        finalArray[i] = numbers[random.Next(0, numbers.Length)];
+        Console.Write(finalArray[i]);
+      }
