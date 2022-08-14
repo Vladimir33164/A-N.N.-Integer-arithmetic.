018f4de134efaaad9e5055b6a7c7c25ad47aa23a
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-14 19:41:20 +0300

    дано четырёхзначное число. получить сумму и произведение

---
+++ A2.10/Program.cs
@@ -0,0 +1,14 @@
+﻿//A2.10. Дано четырехзначное число. Найти:
+//а) сумму его цифр;
+//б) произведение его цифр.
+
+int num, sum = 0, pro = 1;
+Console.WriteLine("Enter a four-digit number!");
+num = Convert.ToInt32(Console.ReadLine());
+while (num > 0)
+{
+sum += num % 10;
+pro *= num % 10;
+num = (num - num % 10) / 10;
+}
+Console.WriteLine($"Sum {sum}, composition {pro}");
