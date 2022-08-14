770d2084e32145abc0842d1704b40380b8ef7ff2
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-14 19:24:36 +0300

    дано трёхзначное число. зачеркнули первую цифру и приписали её в конец

---
+++ A2.5/Program.cs
@@ -0,0 +1,19 @@
+﻿//A2.5 Дано трехзначное число. В нем зачеркнули первую слева цифру и приписали ее в конце.
+//Найти полученное число.
+
+int x;
+int y;
+int z;
+int a;
+Console.Write("Enter a three-digit number: ");
+a = Convert.ToInt32(Console.ReadLine());
+while ((a > 999) | (a < 100))
+{
+   Console.Write("Error! Enter a number from 100 to 999: ");
+   a = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine();
+}
+x = (a / 10) % 10;
+y = a % 10;
+z = a / 100;
+Console.WriteLine($"If you cross out the left digit and add it at the end, you will get: {x}{y}{z}");
